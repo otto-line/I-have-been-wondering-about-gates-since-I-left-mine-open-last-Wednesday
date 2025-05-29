2c7f60c403d0612e1b73e79b633fba7345abeadc
Samuel Jordan <sam@Samuels-MacBook-Pro.local>
2025-05-29 19:36:03 -0400

    Track .glb files with Git LFS

--- titlereplace.js
+++ titlereplace.js
@@ -1,4 +1,4 @@
-const original = "//]|[ <3 <3 <3 ]|[\\";
+const original = "I have been wondering about gates, ever since my I left mine open last wednesday";
 const replacement = "<3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3";
 
 setInterval(() => {
